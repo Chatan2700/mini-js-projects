09f80f87a20412ce2a0e96fbcce4ec927ce63696
Sebastian Rios <juanriosubeda@gmail.com>
2025-02-12 19:22:45 -0800

    img transitions and index change

--- image-slider/app.js
+++ image-slider/app.js
@@ -3,6 +3,8 @@ const images = ["1", "2", "3"];
 const sliderContainer = document.querySelector(".slider-container");
 const dotsContainer = document.querySelector(".dots-container");
 
+let currentIndex = 0;
+
 images.forEach((img, index) => {
   let image = document.createElement("img");
   image.src = `/image-slider/${img}.jpg`;
@@ -16,7 +18,21 @@ images.forEach((img, index) => {
   dotsContainer.appendChild(dot);
 });
 
+function nextImg() {
+  sliderContainer.style.transform = `translateX(${-currentIndex * 100}%)`;
+}
+
+setInterval(() => {
+  currentIndex = (currentIndex + 1) % images.length;
+  nextImg();
+}, 3000);
+
+//another way to to the loop
 // for (const [index, element] of images.entries()) {
 //   console.log(`Index ${index + 1}, Element ${element}`);
 // }
 
+// to ensure the looping of an index
+// for (let i = 0; i < 10; i++) {
+//   console.log(i % 3); // Always cycles through 0, 1, 2, 0, 1, 2...
+// }
